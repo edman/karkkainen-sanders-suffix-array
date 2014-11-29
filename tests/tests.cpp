47e34e9175180e634b54edc23ae9696c676c6966
Edman P. Anjos <edmanjos@gmail.com>
2014-11-29 23:10:21 +0900

    Add more unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -36,16 +36,33 @@ class KarkkainenSandersTest : public testing::Test {
             p12 = new int [n12];
             for (int i = 1, j = 0; j < n12; i += i % 3, j++)
                 p12[j] = i;
+
+            n_ = 5;
+            n0_ = (n_ + 2) / 3, n1_ = n0_, n2_ = n_ / 3;
+            n12_ = n1_ + n2_;
+            sigma_ = 5;
+
+            int s_[] = {5,5,5,5,5,0,0,0};
+            S_ = new int[n_ + 3];
+            memcpy(S_, s_, sizeof(int) * (n_ + 3));
+
+            p12_ = new int [n12_];
+            for (int i = 1, j = 0; j < n12_; i += i % 3, j++)
+                p12_[j] = i;
         }
-        int *S;
-        int *p12;
+        int *S, *S_;
+        int *p12, *p12_;
         int n, n1, n2, n0, n12;
-        int sigma;
+        int n_, n1_, n2_, n0_, n12_;
+        int sigma, sigma_;
 };
 
 TEST_F(KarkkainenSandersTest, TripleSorting) {
     sort_triples(S, p12, n12, sigma);
     ASSERT_EQ(true, is_sorted(S, p12, n12));
+
+    sort_triples(S_, p12_, n12_, sigma_);
+    ASSERT_EQ(true, is_sorted(S_, p12_, n12_));
 }
 
 TEST_F(KarkkainenSandersTest, AssignNames) {
@@ -57,6 +74,15 @@ TEST_F(KarkkainenSandersTest, AssignNames) {
         if (p12[i] % 3 == 1)
             EXPECT_EQ(expected_names[p12[i] / 3], names[i]);
         else EXPECT_EQ(expected_names[p12[i] / 3 + n1], names[i]);
+
+    int expected_names_[] = {2,1,2};
+    int names_[n12_];
+    sort_triples(S_, p12_, n12_, sigma_);
+    assign_names(S_, names_, p12_, n12_);
+    for (int i = 0; i < n12_; ++i)
+        if (p12_[i] % 3 == 1)
+            EXPECT_EQ(expected_names_[p12_[i] / 3], names_[i]);
+        else EXPECT_EQ(expected_names_[p12_[i] / 3 + n1_], names_[i]);
 }
 
 TEST_F(KarkkainenSandersTest, SuffixArray) {
@@ -65,6 +91,12 @@ TEST_F(KarkkainenSandersTest, SuffixArray) {
     karkkainen_sanders_sa(S, sa, n, sigma);
     for (int i = 0; i < n; ++i)
         EXPECT_EQ(expected_sa[i], sa[i]);
+
+    int expected_sa_[] = {4,3,2,1,0};
+    int sa_[n];
+    karkkainen_sanders_sa(S_, sa_, n_, sigma_);
+    for (int i = 0; i < n_; ++i)
+        EXPECT_EQ(expected_sa_[i], sa_[i]);
 }
 
 class KarkkainenSandersTest2 : public testing::Test {
